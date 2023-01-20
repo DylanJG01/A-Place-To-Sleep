6d8a4415283abdeb0f1cdbf8f51e94e079affdbf
Dylan Godeck <Dylanj.Godeck@gmail.com>
2023-01-20 13:48:36 -0800

    migration and demo seeds

---
+++ backend/db/seeders/20230120213843-demo-user.js
@@ -0,0 +1,38 @@
+'use strict';
+const bcrypt = require("bcryptjs");
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  
+}
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    options.tableName = 'Users';
+    return queryInterface.bulkInsert(options, [
+      {
+        email: 'demo@user.io',
+        username: 'DemoUser1',
+        hashedPassword: bcrypt.hashSync('thebestpassword')
+      },
+      {
+        email: 'the@dog.io',
+        username: 'DoggoMcDogDog',
+        hashedPassword: bcrypt.hashSync('treats&pets4lyfe')
+      },
+      {
+        email: 'TheLastWord@doom.yesterday',
+        username: 'Snuffles',
+        hashedPassword: bcrypt.hashSync('TheEndApproaches')
+      },
+    ], {})
+  },
+
+  async down (queryInterface, Sequelize) {
+    options.tableName = 'Users';
+    const Op = Sequelize.Op;
+    return queryInterface.bulkDelete(options, {
+      username: { [Op.in]: ['DemoUser1', 'DoggoMcDogDog', 'Snuffles']}
+    })
+  }
+};
