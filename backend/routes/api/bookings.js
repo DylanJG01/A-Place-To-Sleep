f4ecdb625f239526cb3f16c7fb00c3be74c38c46
Dylan Godeck <Dylanj.Godeck@gmail.com>
2023-01-26 15:13:34 -0800

    added booking router to index and added GET /bookings/current

--- backend/routes/api/bookings.js
+++ backend/routes/api/bookings.js
@@ -1,16 +1,37 @@
 const express = require('express')
 const router = express.Router();
-const { Spot, Review, Booking } = require('../../db/models');
+const { Spot, Review, Booking, SpotImage, User } = require('../../db/models');
 const { requireAuth } = require('../../utils/auth');
 
 router.get(
     '/current',
     requireAuth,
     async (req, res, next) => {
-
         
+        let userBookings = await Booking.findAll({
+            where: {userId: req.user.id},
+        });
+        
+        userBookings = userBookings.map(el => el.toJSON())
+
+        let spots = [];
+
+        for(let booking of userBookings){
+           let spot = await Spot.findByPk(booking.spotId)
+           spots.push(spot.toJSON())
+        }
+
+        userBookings = userBookings.map((el, index) => {
+            el.spot = spots[index];
+            return el;
+        })
+
+        res.json(userBookings)
     }
 )
+
+
+
 
 
 module.exports = router;
