1b1a418be312fc67ad93032c96a3e3b7acb49ad9
Dylan Godeck <Dylanj.Godeck@gmail.com>
2023-02-10 10:23:11 -0800

    Phase 2 complete

--- backend/app.js
+++ backend/app.js
@@ -66,12 +66,11 @@ app.use((err, _req, _res, next) => {
 
 app.use((err, _req, res, _next) => {
     res.status(err.status || 500);
-    // console.error(err.errors); //remove later
-    
+    // console.error(err); //remove later
     res.json({
         title: err.title || 'Server Error',
         message: err.message,
-        errors: {...err.errors},
+        errors: err.errors,
         stack: isProduction ? null : err.stack
     });
 });
