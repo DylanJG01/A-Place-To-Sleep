0008bfc9d2f86d37762439530f95d34cad1ab908
Dylan Godeck <Dylanj.Godeck@gmail.com>
2023-02-12 19:42:34 -0800

    working on modal css

--- frontend/src/components/LoginFormModal/index.js
+++ frontend/src/components/LoginFormModal/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import * as sessionActions from "../../store/session";
 import { useDispatch } from "react-redux";
 import { useModal } from "../../context/Modal";
@@ -10,6 +10,7 @@ function LoginFormModal() {
     const [password, setPassword] = useState("");
     const [errors, setErrors] = useState([]);
     const { closeModal } = useModal();
+    const [disableBtn, setDisableBtn] = useState(true)
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -24,34 +25,43 @@ function LoginFormModal() {
             );
     };
 
+    useEffect(() => {
+        if (credential.length < 4
+        || password.length < 6) return setDisableBtn(true)
+        return setDisableBtn(false)
+    },[credential, password])
+
     return (
         <>
             <h1>Log In</h1>
-            <form onSubmit={handleSubmit}>
+            <form onSubmit={handleSubmit} className="login-form">
                 <ul>
                     {errors.map((error, idx) => (
                         <li key={idx}>{error}</li>
                     ))}
                 </ul>
                 <label>
-                    Username or Email
                     <input
+                        className="credential"
                         type="text"
                         value={credential}
                         onChange={(e) => setCredential(e.target.value)}
                         required
+                        placeholder="Username or Email"
+
                     />
                 </label>
-                <label>
-                    Password
+                <label>                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                
                     <input
+                        className="password"
                         type="password"
                         value={password}
                         onChange={(e) => setPassword(e.target.value)}
                         required
+                        placeholder="Password"
                     />
                 </label>
-                <button type="submit">Log In</button>
+                <button type="submit" disabled={disableBtn}>Log In</button>
             </form>
         </>
     );
--- frontend/src/components/SignupFormModal/index.js
+++ frontend/src/components/SignupFormModal/index.js
@@ -1,7 +1,8 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { useModal } from "../../context/Modal";
 import * as sessionActions from "../../store/session";
+
 import './SignupForm.css';
 
 function SignupFormModal() {
@@ -14,6 +15,7 @@ function SignupFormModal() {
     const [confirmPassword, setConfirmPassword] = useState("");
     const [errors, setErrors] = useState([]);
     const { closeModal } = useModal();
+    const [disableBtn, setDisableBtn] = useState(true)
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -29,6 +31,19 @@ function SignupFormModal() {
         return setErrors(['Confirm Password field must be the same as the Password field']);
     };
 
+
+     useEffect(() => {
+        if (username.length < 4
+         ||email.length === 0
+         ||firstName.length === 0
+         ||lastName.length === 0
+         ||password.length < 6
+         ||confirmPassword.length < 6
+         ||confirmPassword !== password ) return setDisableBtn(true)
+
+        return setDisableBtn(false);
+    }, [password, username, firstName, lastName, password, confirmPassword])
+
     return (
         <>
             <h1>Sign Up</h1>
@@ -36,61 +51,62 @@ function SignupFormModal() {
                 <ul>
                     {errors.map((error, idx) => <li key={idx}>{error}</li>)}
                 </ul>
-                <label>
-                    Email
+                <label className="email">
                     <input
                         type="text"
                         value={email}
                         onChange={(e) => setEmail(e.target.value)}
                         required
+                        placeholder="Email"
                     />
                 </label>
-                <label>
-                    Username
+                <label className="username">
+                    
                     <input
                         type="text"
                         value={username}
                         onChange={(e) => setUsername(e.target.value)}
                         required
+                        placeholder="Username"
                     />
                 </label>
-                <label>
-                    First Name
+                <label className="first-name">
                     <input
                         type="text"
                         value={firstName}
                         onChange={(e) => setFirstName(e.target.value)}
                         required
+                        placeholder="First name"
                     />
                 </label>
-                <label>
-                    Last Name
+                <label className="last-name">
                     <input
                         type="text"
                         value={lastName}
                         onChange={(e) => setLastName(e.target.value)}
                         required
+                        placeholder="Last Name"
                     />
                 </label>
-                <label>
-                    Password
+                <label className="password">
                     <input
                         type="password"
                         value={password}
                         onChange={(e) => setPassword(e.target.value)}
                         required
+                        placeholder="Password"
                     />
                 </label>
-                <label>
-                    Confirm Password
+                <label className="confirm-password">
                     <input
                         type="password"
                         value={confirmPassword}
                         onChange={(e) => setConfirmPassword(e.target.value)}
                         required
+                        placeholder="Confirm password"
                     />
                 </label>
-                <button type="submit">Sign Up</button>
+                <button type="submit" disabled={disableBtn}>Sign Up</button>
             </form>
         </>
     );
