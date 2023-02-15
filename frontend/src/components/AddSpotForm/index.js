115f71aa8a07482faf13449d394ecd06f40711ac
Dylan Godeck <Dylanj.Godeck@gmail.com>
2023-02-14 21:55:31 -0800

    addSpotForm images and validations added, and spotValidator helper function made

--- frontend/src/components/AddSpotForm/index.js
+++ frontend/src/components/AddSpotForm/index.js
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from "react"
 import {useDispatch, useSelector} from 'react-redux'
 import { addSpotThunk } from "../../store/spots"
+import _spotValidator from './_spotValidator'
 import "./AddSpotForm.css"
+
 const AddSpotForm = () => {
     const dispatch = useDispatch()
     const [country, setCountry] = useState('')//country
@@ -13,19 +15,24 @@ const AddSpotForm = () => {
     const [description, setDescription] = useState('')//description
     const [title, setTitle] = useState('')//name
     const [price, setPrice] = useState('')
-    // const [pictures, setPictures] = useState([])
+    const [pictures, setPictures] = useState([]) 
     const [errors, setErrors] = useState([])
-    const [disableBtn, setDisableBtn] = useState(true)
+    const [picture1, setPicture1] = useState('')
+    const [picture2, setPicture2] = useState('')
+    const [picture3, setPicture3] = useState('')
+    const [picture4, setPicture4] = useState('')
+    const [picture5, setPicture5] = useState('')
+    const [displayErrors, setDisplayErrors] = useState(false)
 
     const user = useSelector(state => state.session.user)
 
-    // useEffect(() => {
-
-    // })
-
     const handleSubmit = (e) => {
         e.preventDefault();
-        setErrors([])
+        if(errors.length){
+            setDisplayErrors(true)
+            console.log(errors)
+            return
+        }
         const spot = {
             country,
             address,
@@ -35,16 +42,35 @@ const AddSpotForm = () => {
             lng: +longitude,
             description,
             name: title,
-            price: +price
+            price: +price,
+            pictures
         }
         return dispatch(addSpotThunk(user, spot))
-            .catch(
-                async (res) => {
-                    const data = await res.json();
-                    if (data && data.errors) setErrors(data.errors);
-                }
-            );
+               .catch(
+                    async (res) => {
+                        const data = await res.json();
+                        if (data && data.errors) setErrors(data.errors);
+                    }
+        );
     }
+
+    useEffect(() => {
+        setPictures([picture1, picture2, picture3, picture4, picture5])
+    },[picture1, picture2, picture3, picture4, picture5])
+
+    useEffect(() => {
+        setErrors(_spotValidator({
+            country,
+            address,
+            city,
+            state,
+            lat: +latitude,
+            lng: +longitude,
+            description,
+            name: title,
+            price: +price
+        }))
+    }, [country, address, city, state, latitude, longitude, description, title, price])
 
     if (!user) return (
         <>Please Login to make create a new spot</>
@@ -58,14 +84,20 @@ const AddSpotForm = () => {
                     {!!errors.length && errors.map((error, idx) => {})}
                 </ul>
                 <label>
-                    <h5>Country</h5>
+                    <div>
+                    <h5>
+                        Country{displayErrors && errors.includes("Country") && 
+                        // (<p>Country is required</p>)}
+                             (<>  Country is required</>)}
+                    </h5>
+                    </div>
                     <input 
                         className="country"
                         type="text"
                         value={country}
                         onChange={(e) => setCountry(e.target.value)}
                         placeholder="Country"
-                        required
+                        // required
                     />
                 </label>
                 <label>
@@ -76,7 +108,7 @@ const AddSpotForm = () => {
                         value={address}
                         onChange={(e) => setAddress(e.target.value)}
                         placeholder="Street Address"
-                        required
+                        // required
                     />
                 </label>
                 <label>
@@ -86,7 +118,7 @@ const AddSpotForm = () => {
                         type="text"
                         value={city}
                         onChange={(e) => setCity(e.target.value)}
-                        required
+                        // required
                         placeholder="City"
                     />
                 </label>
@@ -97,7 +129,7 @@ const AddSpotForm = () => {
                         type="text"
                         value={state}
                         onChange={(e) => setState(e.target.value)}
-                        required
+                        // required
                         placeholder="State"
                     />
                 </label>
@@ -108,7 +140,7 @@ const AddSpotForm = () => {
                         type="text"
                         value={latitude}
                         onChange={(e) => setLatitude(e.target.value)}
-                        required
+                        // required
                         placeholder="Latitude"
                     />
                 </label>
@@ -119,7 +151,7 @@ const AddSpotForm = () => {
                         type="text"
                         value={longitude}
                         onChange={(e) => setLongitude(e.target.value)}
-                        required
+                        // required
                         placeholder="Longitude"
                     />
                 </label>
@@ -130,7 +162,7 @@ const AddSpotForm = () => {
                         type="textarea"
                         value={description}
                         onChange={(e) => setDescription(e.target.value)}
-                        required
+                        // required
                         placeholder="Description"
                     />
                 </label>
@@ -141,8 +173,8 @@ const AddSpotForm = () => {
                         type="text"
                         value={title}
                         onChange={(e) => setTitle(e.target.value)}
-                        required
-                        placeholder="Title"
+                        // required
+                        placeholder="Name of your spot"
                     />
                 </label>
                 <label>
@@ -152,19 +184,63 @@ const AddSpotForm = () => {
                         type="text"
                         value={price}
                         onChange={(e) => setPrice(e.target.value)}
-                        required
-                        placeholder="Price"
+                        // required
+                        placeholder="Price per night (USD)"
+                    />
+                </label>
+                <label>
+                    <h4>Liven up your spots with photos</h4>
+                    <input
+                        className="img-link"
+                        type="text"
+                        value={picture1}
+                        onChange={(e) => setPicture1(e.target.value)}
+                        // required
+                        placeholder="Preview Image Url"
+                    />
+                </label>
+                <label>
+                    <input
+                        className="img-link"
+                        type="text"
+                        value={picture2}
+                        onChange={(e) => setPicture2(e.target.value)}
+                        
+                        placeholder="Image Url"
+                    />
+                </label>
+                <label>
+                    <input
+                        className="img-link"
+                        type="text"
+                        value={picture3}
+                        onChange={(e) => setPicture3(e.target.value)}
+                      
+                        placeholder="Image Url"
+                    />
+                </label>
+                <label>
+                    <input
+                        className="img-link"
+                        type="text"
+                        value={picture4}
+                        onChange={(e) => setPicture4(e.target.value)}
+                       
+                        placeholder="Image Url"
+                    />
+                </label>
+                <label>
+                    <input
+                        className="img-link"
+                        type="text"
+                        value={picture5}
+                        onChange={(e) => setPicture5(e.target.value)}
+                        placeholder="Image Url"
                     />
                 </label>
                 <div>
                 <button type="submit" disabled={false}>Create Spot</button>
                 </div>
-                {/* <label>
-                    <h4>Liven up your spots with photos</h4>
-                    <input
-                        className=""
-                    />
-                </label> */}
             </form>
         </>
     )
