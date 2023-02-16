8af4a6392cf0163a6760a021631ac8f7c763c284
Dylan Godeck <Dylanj.Godeck@gmail.com>
2023-02-16 15:21:08 -0800

    Added real img seed data and added a preview image from every demo-spot.

--- backend/db/seeders/20230124045346-demo-spotimages-data.js
+++ backend/db/seeders/20230124045346-demo-spotimages-data.js
@@ -10,23 +10,28 @@ module.exports = {
     return queryInterface.bulkInsert(options, [
       { 
         spotId: 1,
-        url: 'www.somewhere1.com',
+        url: 'https://img.wallpaper.sc/android/images/1440x1280/android-1440x1280-wallpaper_00674.jpg',
         preview: true
       },
       { 
         spotId: 1,
-        url: 'www.somewhere2.com',
+        url: 'https://i.pinimg.com/originals/6f/f2/c0/6ff2c02906bdf3afba9cbb91613fefa1.jpg',
         preview: false
       },
       {
         spotId: 2,
-        url: 'www.spot2img1.com',
-        preview: false
+        url: 'https://i.pinimg.com/originals/db/44/f7/db44f7db6c4c533f760086c172899228.jpg',
+        preview: true
       },
       {
         spotId: 2,
-        url: 'www.spot2img2.com',
+        url: 'https://i.pinimg.com/originals/3d/d4/f9/3dd4f97f9c90fb2315d8b1782ab15934.jpg',
         preview: false
+      },
+      {
+        spotId: 3,
+        url: 'https://i.pinimg.com/originals/14/78/a0/1478a015a32a4779da586013b5007544.jpg',
+        preview: true
       },
     ])
   },
