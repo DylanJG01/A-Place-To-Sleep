30b672e4d5a8365125a2c7cf4c97542f72b45fef
DylanJG01 <DylanJ.Godeck@gmail.com>
2023-06-22 14:03:05 -0700

    spot image change

--- backend/db/seeders/20230124045346-demo-spotimages-data.js
+++ backend/db/seeders/20230124045346-demo-spotimages-data.js
@@ -8,24 +8,34 @@ module.exports = {
   async up (queryInterface, Sequelize) {
     options.tableName = 'SpotImages';
     return queryInterface.bulkInsert(options, [
-      { 
+      {
         spotId: 1,
-        url: 'https://img.wallpaper.sc/android/images/1440x1280/android-1440x1280-wallpaper_00674.jpg',
+        url: 'https://images.nightcafe.studio/jobs/PfrOnSctTPzfAvoV0PZ6/PfrOnSctTPzfAvoV0PZ6--14--4wz28.jpg?tr=w-1600,c-at_max',
         preview: true
       },
-      { 
+      {
         spotId: 1,
-        url: 'https://i.pinimg.com/originals/6f/f2/c0/6ff2c02906bdf3afba9cbb91613fefa1.jpg',
+        url: 'https://images.nightcafe.studio/jobs/choU5x89JzajfGF3ZP34/choU5x89JzajfGF3ZP34--1--49ouu.jpg?tr=w-1600,c-at_max',
+        preview: false
+      },
+      {
+        spotId: 1,
+        url: 'https://images.nightcafe.studio/jobs/PfrOnSctTPzfAvoV0PZ6/PfrOnSctTPzfAvoV0PZ6--3--s7irw.jpg?tr=w-1600,c-at_max',
+        preview: false
+      },
+      {
+        spotId: 1,
+        url: 'https://images.nightcafe.studio/jobs/PfrOnSctTPzfAvoV0PZ6/PfrOnSctTPzfAvoV0PZ6--16--2itkm.jpg?tr=w-1600,c-at_max',
         preview: false
       },
       {
         spotId: 2,
-        url: 'https://i.pinimg.com/originals/db/44/f7/db44f7db6c4c533f760086c172899228.jpg',
+        url: 'https://i.pinimg.com/736x/1c/98/37/1c9837cf427911808c2f03d03e8919ff.jpg',
         preview: true
       },
       {
         spotId: 2,
-        url: 'https://i.pinimg.com/originals/3d/d4/f9/3dd4f97f9c90fb2315d8b1782ab15934.jpg',
+        url: 'https://images.nightcafe.studio/jobs/7ZvCuDzmP42DVQ0WbI4X/7ZvCuDzmP42DVQ0WbI4X--1--5yvjy.jpg?tr=w-1600,c-at_max',
         preview: false
       },
       {
