9d5e613d2aa18064d737f981dfc5a5c0258a3c36
DylanJG01 <DylanJ.Godeck@gmail.com>
2024-03-30 20:56:56 -0700

    cleaning up more commented out code

--- frontend/src/components/DeleteReview/DeleteReviewModal.js
+++ frontend/src/components/DeleteReview/DeleteReviewModal.js
@@ -1,26 +1,19 @@
-import React, { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
+
+import { useDispatch } from "react-redux";
 import { useModal } from "../../context/Modal";
-// import './DeleteSpotModal.css'
 import { deleteReviewThunk } from "../../store/reviews";
-// import { getSingleSpot } from '../../store/spots';
-// import { useParams } from 'react-router-dom'
+
 
 export default function DeleteReviewModal({ review }) {
     const dispatch = useDispatch();
     const { closeModal } = useModal()
-    // const { spotId } = useParams();
-    // const reviews = useSelector(state => state.reviews)
-    // console.log(spot)
+
 
     const confirmDelete = (review) => {
         dispatch(deleteReviewThunk(review))
         .then(closeModal)
     }
-//    useEffect(() => {
-//     dispatch(getSingleSpot(spotId))
 
-//    }, [dispatch, reviews])
 
     return (
         <>
